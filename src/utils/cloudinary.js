8bf3bfa30b4b89baec14ca4bc121ceda6183c380
Zeel Dhaduk <dhadukzeel29@gmail.com>
2025-05-30 21:52:53 +0530

    fixed error in register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,24 +1,26 @@
 import { v2 as cloudinary } from 'cloudinary';
-import fs from fs
+import fs from "fs";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRE 
+        api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath) return null
         //upload the file on cloudinary
-        cloudinary.uploader.upload(localFilePath, {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-
-        console.log("File has been uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     }catch (error){
-        fs.inlinkSync(localFilePath)
+        fs.unlinkSync(localFilePath)
         //remove the locally saved temporary file as the upload operation got failed
         return null;
     }
